07da807a6fd0d5375979bf164f74faa82525c1be
luisejv <ljauregui@pucp.pe>
2019-10-19 13:44:08 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Proyecto_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_2.dir/main.cpp.obj"
+  "Proyecto_2.exe"
+  "Proyecto_2.exe.manifest"
+  "Proyecto_2.pdb"
+  "libProyecto_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
